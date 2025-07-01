333aef5201a4b97856c1472026420141f724e578
abdultalha0862 <20211a0405@bvrit.ac.in>
2025-07-01 15:08:12 +0530

    Add Contact component with welcome alert script

---
+++ Image Script and Link/app/contact/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Script from 'next/script'
+
+const contact = () => {
+  return (
+    <div>
+      <Script id="welcome-alert">
+        {`alert("You are Welcome")`}
+      </Script>
+    </div>
+  )
+}
+
+export default contact
