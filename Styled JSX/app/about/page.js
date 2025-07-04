d8527e41aa22cab2bad96e07a258bf0e1be29088
abdultalha0862 <20211a0405@bvrit.ac.in>
2025-07-04 15:38:33 +0530

    Add About component with basic structure and styling

---
+++ Styled JSX/app/about/page.js
@@ -0,0 +1,27 @@
+"use client"
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1>This is about Me</h1>
+        <p>Hey This is about Me</p>
+
+
+        <style jsx>{`
+        .container{
+            background-color:red;
+            color:green;
+        }
+        `}</style>
+        
+        <div className="container">
+            Hey I am good
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
